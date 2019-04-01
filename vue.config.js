db33e7eb5d38d0ba193d12de1032541226f125c9
杨晓东 <yacoda@qq.com>
2019-04-01 08:57:37 +0800

    .

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,14 @@
 
 module.exports = {
-    devServer: {
-      proxy: {
-        '/': {
-            target: 'http://192.168.102.221:7080/',
-            changeOrigin: true,
-            ws:false
-          }
-      }
+  devServer: {
+    proxy: {
+      '/': {
+          target: 'http://192.168.102.221:7080/',
+          changeOrigin: true,
+          ws:false
+        }
     }
-  }
+  },
+
+  publicPath: ''
+}
