1df803893d639c143958a13706e36ddfd459e24e
杨晓东 <33110737+nofish@users.noreply.github.com>
2019-04-02 21:39:42 +0800

    .

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import router from './router'
 import store from './store'
 import  $  from "./utils/index.js"
 import  http  from "./utils/data.js"
-
+import console from './utils/console';
+// console()
 Vue.prototype.$ = $
 Vue.prototype.http = http
 Vue.config.productionTip = false
