842ddeeb2ce288ccbad0522668be8b8c55b11a22
杨晓东 <33110737+nofish@users.noreply.github.com>
2019-04-10 15:36:12 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,222 +1,237 @@
-
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
     <Model v-if="showModel" :content="cityList ? cityList[localCity].cityName : ''" @handelModel='handelModel' />
     <p class="toast" v-if="toast">{{toast}}</p>
   </div>
 </template>
 
 <script>
-import axios from "axios";
-import API from "@/utils/api";
-import Model from '@/components/Model'
-import { mapState, mapMutations,mapActions } from "vuex";
-import { setTimeout } from 'timers';
-export default {
-  name: "app",
-  data(){
-    return{
-      showModel:false
-    }
-  },
-  components:{
-    Model,
-    localCity:-1 //定位的城市索引
-  },
-  created: function() {
-    this.httpQueryCityList();
-    this.httpQueryMapBaseData();
-  },
-  mounted(){
-    this.location();
+  import axios from "axios";
+  import API from "@/utils/api";
+  import Model from '@/components/Model'
+  import {
+    mapState,
+    mapMutations,
+    mapActions
+  } from "vuex";
+  import {
+    setTimeout
+  } from 'timers';
+  export default {
+    name: "app",
+    data() {
+      return {
+        showModel: false
+      }
+    },
+    components: {
+      Model,
+      localCity: -1 //定位的城市索引
+    },
+    created: function () {
+      this.httpQueryCityList();
+      this.httpQueryMapBaseData();
+    },
+    mounted() {
+      this.location();
 
-        var lastTouchEnd = 0;
-    document.addEventListener('touchstart', function(event) {
+      var lastTouchEnd = 0;
+      document.addEventListener('touchstart', function (event) {
         if (event.touches.length > 1) {
-            event.preventDefault();
+          event.preventDefault();
         }
-    });
-    document.addEventListener('touchend', function(event) {
+      });
+      document.addEventListener('touchend', function (event) {
         var now = (new Date()).getTime();
         if (now - lastTouchEnd <= 300) {
-            event.preventDefault();
+          event.preventDefault();
         }
         lastTouchEnd = now;
-    }, false);
+      }, false);
 
-    // 阻止双指放大
-    document.addEventListener('gesturestart', function(event) {
+      // 阻止双指放大
+      document.addEventListener('gesturestart', function (event) {
         event.preventDefault();
-    });
-  },
-  methods: {
-    ...mapMutations(["assign","mapBaseData","resetAllState","mapDataChangelatitudeAndLongitude",'currentCityChange','currentCityAddConfirm']),
-    ...mapActions(['assignAsync']),
-    location(){
-      if(navigator.geolocation){  //判断是否支持Geolocation API
-	    navigator.geolocation.getCurrentPosition(res=>{
-      },err=>{
-        console.log(err)
-      },{ 
-enableHighAccuracy:true, //设置提升定位的精准度
-	maximumAge:0,  //禁用缓存
-	timeout:30000  //开始获取定位信息30秒后超时
-      })
-    }
-    },
-    httpQueryCityList: function() {
-      let _this = this;
-      axios.post(API["queryCityList"]).then(res => {
-        if (res.data.code == 0) {
-          _this.assign({
-            key: "cityList",
-            value: res.data.data
-          });
-          _this.loadCity()
-        }
       });
     },
-    httpQueryMapBaseData: function() {
-      axios.post(API["queryMapBaseData"], { cityId: this.currentCity.cityId }).then(res => {
-        if (res.data.code == 0) {
-          this.mapBaseData(res.data.data)
-           this.assignAsync({
-            key: "mapBaseDataReady",
-            value: true
-          });
+    methods: {
+      ...mapMutations(["assign", "mapBaseData", "resetAllState", "mapDataChangelatitudeAndLongitude",
+        'currentCityChange', 'currentCityAddConfirm'
+      ]),
+      ...mapActions(['assignAsync']),
+      location() {
+        if (navigator.geolocation) { //判断是否支持Geolocation API
+          navigator.geolocation.getCurrentPosition(res => {}, err => {
+            console.log(err)
+          }, {
+            enableHighAccuracy: true, //设置提升定位的精准度
+            maximumAge: 0, //禁用缓存
+            timeout: 30000 //开始获取定位信息30秒后超时
+          })
         }
-      });
-    },
-    handelModel(e){
-      this.showModel = false;
-      if(e){
-        //点击确定
-        this.currentCityChange(this.localCity)
-      }else{
-        //点击取消
-         this.currentCityAddConfirm()
-      }
-    },
-    loadCity() {
-      if (this.currentCity.confirm == undefined) {
+      },
+      httpQueryCityList: function () {
+        let _this = this;
+        axios.post(API["queryCityList"]).then(res => {
+          if (res.data.code == 0) {
+            _this.assign({
+              key: "cityList",
+              value: res.data.data
+            });
+            _this.loadCity()
+          }
+        });
+      },
+      httpQueryMapBaseData: function () {
+        axios.post(API["queryMapBaseData"], {
+          cityId: this.currentCity.cityId
+        }).then(res => {
+          if (res.data.code == 0) {
+            this.mapBaseData(res.data.data)
+            this.assignAsync({
+              key: "mapBaseDataReady",
+              value: true
+            });
+          }
+        });
+      },
+      handelModel(e) {
+        this.showModel = false;
+        if (e) {
+          //点击确定
+          this.currentCityChange(this.localCity)
+        } else {
+          //点击取消
+          this.currentCityAddConfirm()
+        }
+      },
+      loadCity() {
+        if (this.currentCity.confirm == undefined) {
           if (localStorage.currentCity != undefined) {
-              this.assign({
-                  key: 'currentCity',
-                  value: JSON.parse(localStorage.currentCity)
-              })
+            this.assign({
+              key: 'currentCity',
+              value: JSON.parse(localStorage.currentCity)
+            })
           }
           this.mapDataChangelatitudeAndLongitude({
-              latitude: this.currentCity.latitude,
-              longitude: this.currentCity.longitude
+            latitude: this.currentCity.latitude,
+            longitude: this.currentCity.longitude
           })
           this.getLocation()
-      }
-  },
-    getLocation() {
+        }
+      },
+      getLocation() {
         var myCity = new BMap.LocalCity();
         myCity.get(res => {
-            let localCity = this.cityList.findIndex(city => {
-                return city.baiduCode == res.code;
-            });
-            if (this.cityList[localCity].cityId != this.currentCity.cityId && localCity != -1) {
-                this.localCity = localCity;
-                this.showModel = true;
-            }
+          let localCity = this.cityList.findIndex(city => {
+            return city.baiduCode == res.code;
+          });
+          if (this.cityList[localCity].cityId != this.currentCity.cityId && localCity != -1) {
+            this.localCity = localCity;
+            this.showModel = true;
+          }
         });
-    }
-  },
-  computed:mapState(['currentCity','toast','cityList']),
-  watch:{
-    currentCity(){
-      this.httpQueryMapBaseData();
-      this.resetAllState();
+      }
     },
-    toast(val){
-      if(val){
-        setTimeout(()=>{
-          this.assign({
-            key:'toast',
-            value:null
-          })
-        },3000)
+    computed: mapState(['currentCity', 'toast', 'cityList']),
+    watch: {
+      currentCity() {
+        this.httpQueryMapBaseData();
+        this.resetAllState();
+      },
+      toast(val) {
+        if (val) {
+          setTimeout(() => {
+            this.assign({
+              key: 'toast',
+              value: null
+            })
+          }, 3000)
+        }
       }
     }
-  }
-};
+  };
 </script>
 
 <style scoped>
-.toast{
-  position: fixed;
-  left: 0;
-  right: 0;
-  bottom: 30vh;
-  margin: auto;
-  z-index: 999;
-  font-size: 14px;
-  text-align: center;
-  width: 70vw;
-  background: rgba(0, 0, 0, 0.5);
-  color: #eee;
-  line-height: 1.4;
-  padding: 5px 0;
-  border-radius: 10px;
-}
+  .toast {
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 30vh;
+    margin: auto;
+    z-index: 999;
+    font-size: 14px;
+    text-align: center;
+    width: 70vw;
+    background: rgba(0, 0, 0, 0.5);
+    color: #eee;
+    line-height: 1.4;
+    padding: 5px 0;
+    border-radius: 10px;
+  }
 </style>
 
 
 <style>
-body,
-ul,
-ol,
-li,
-p,
-h1,
-h2,
-h3,
-h4,
-h5,
-h6,
-form,
-fieldset,
-table,
-td,
-img,
-div,
-dt,
-dl,
-dd,
-blockquote {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-family: "Microsoft Yahei", Arial, Helvetica, sans-serif;
-  word-break: break-all;
-}
-a {
-  color: #000;
-  text-decoration: none;
-}
-a:hover {
-  text-decoration: none;
-  outline: none;
-}
-ul li {
-  list-style: none;
-}
-input {
-  outline: none;
-}
-button {
-  background: none;
-  border: none;
-  outline: none;
-}
-.bottom48 {
-  bottom: 48vw !important;
-}
-.anchorBL{
-  display:none;
-}
+  body,
+  ul,
+  ol,
+  li,
+  p,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  form,
+  fieldset,
+  table,
+  td,
+  img,
+  div,
+  dt,
+  dl,
+  dd,
+  blockquote {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-family: "Microsoft Yahei", Arial, Helvetica, sans-serif;
+    word-break: break-all;
+  }
+
+  a {
+    color: #000;
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: none;
+    outline: none;
+  }
+
+  ul li {
+    list-style: none;
+  }
+
+  input {
+    outline: none;
+  }
+
+  button {
+    background: none;
+    border: none;
+    outline: none;
+  }
+
+  .bottom48 {
+    bottom: 48vw !important;
+  }
+
+  .anchorBL {
+    display: none;
+  }
 </style>
